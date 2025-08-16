9c11943bb786f61a757cccc305df8610e2fea7fd
Aidan Heaslip <sientyhd89@gmail.com>
2025-08-16 17:02:18 +1000

    Initial commit

---
+++ src/main/java/com/veroud/choEazyAnvil/ChoEazyAnvil.java
@@ -0,0 +1,17 @@
+package com.veroud.choEazyAnvil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChoEazyAnvil extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
